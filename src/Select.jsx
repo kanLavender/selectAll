9d093ac309706c863e8b2596ab671cc4a885f8d9
毛钰—029917 <029917@etransfar.com>
2024-11-29 17:12:28 +0800

    添加下拉搜索

--- src/Select.jsx
+++ src/Select.jsx
@@ -5,8 +5,12 @@ const Select = (props) => {
   const { mode, options, placeholder } = props;
   const [selectItem, setSelectItem] = useState(mode === 'multi' ? [] : null);
   const [open, setOpen] = useState(false);
+  const [showClear, setShowClear] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+    // 新增：存储过滤后的选项列表状态
+  const [filteredOptions, setFilteredOptions] = useState(options);
 
-  const handleSearch = (option) => {
+  const onChange = (option) => {
     if (mode === 'multi') {
       // 如果是多选模式，切换选择状态
       const isSelected = selectItem.includes(option);
@@ -14,34 +18,45 @@ const Select = (props) => {
         ? selectItem.filter(item => item !== option)
         : [...selectItem, option];
       setSelectItem(newSelection);
+      setShowClear(!!newSelection);//选中下拉值后再展示清除符号
     } else {
       // 单选模式直接设置选项
       setSelectItem(option);
     }
   };
+  
+ // 添加搜索功能，根据搜索关键词过滤选项
+ const onSearch = (searchTerm) => {
+    setSearchTerm(searchTerm)
+    const filteredOptions = options.filter(option => option.toLowerCase().includes(searchTerm.toLowerCase()));
+    setFilteredOptions(filteredOptions);
+};
 
   const onClear = () => {
     setSelectItem(mode === 'multi' ? [] : null);
+    setShowClear(false);
   };
 
   const onOpen = () => {
     setOpen(!open);
   };
 
+  //修改交互，静态输入框展示下拉符号，点击后展示搜索符号，选中某个选项后展示清除符号
   return (
     <div className='panel'>
-      <div className={`searchInfo ${open ? styles.open : ''}`} onClick={onOpen}>
+      <div className={`searchInfo ${open ? styles.open : ''}`} onClick={onOpen} onChange = {(e)=>onSearch(e.target.value)} >
         {selectItem && (mode === 'multi' ? selectItem.join(", ") : selectItem) || placeholder}
-        <span className='clearIcon' onClick={onClear}>x</span>
+        {showClear && <span className='clearIcon' onClick={onClear}>x</span>} 
+        
         <span className='dropdownArrow' style={{ transform: open ? 'rotate(180deg)' : 'rotate(0deg)' }}></span>
       </div>
       {open && (
         <ul className='options'>
-          {options.map((option) => (
+          {filteredOptions.map((option) => (
             <li
               className={(mode === "multi" ? selectItem.includes(option) : selectItem === option) ? styles.selected : ""}
               key={option}
-              onClick={() => handleSearch(option)}
+              onClick={() => onChange(option)}
             >
               {option}
             </li>
