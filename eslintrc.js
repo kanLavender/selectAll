459dc3287ad5736a8e4b9969e057f3f11632f390
毛钰—029917 <029917@etransfar.com>
2024-12-17 10:06:12 +0800

    提交eslint校验

---
+++ eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "rules":{
+        "semi": ["error", "always"],//要求在语句末尾加上分号
+        "quotes": ["error", "double"],//要求双引号
+        "indent": ["error", 4],//缩进4个空格
+    }
+}
